8bc5598dcb13e0c1d9d3b37eecc898d40d5c5b10
noegigi <33064667+noegigi@users.noreply.github.com>
2019-05-20 15:20:10 +0200

    Premiere feature: Pawn VR + Movement VR

---
+++ Source/FeatureVRLudus/FeatureVRLudusGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FeatureVRLudusGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEATUREVRLUDUS_API AFeatureVRLudusGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
